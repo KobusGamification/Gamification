f06df6428a5e520350af31d2884351fe79251cd8
unknown <leonardo.kobus@NT-01306.hbsisnet.local>
2014-10-02 22:05:41 -0300

    Add configuration for Multiplies Users

--- Gamification/DatabaseAccess/Configuration/UserMap.cs
+++ Gamification/DatabaseAccess/Configuration/UserMap.cs
@@ -3,10 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
 
 namespace DatabaseAccess.Configuration
 {
-    public class UserMap
+    public class UserMap : ConfigurationElement
     {
+        [ConfigurationProperty("mainName", IsKey = true, IsRequired = true)]
+        public string MainName
+        {
+            get
+            {
+                return base["mainName"] as string;
+            }
+            set
+            {
+                base["mainName"] = value;
+            }
+        }
+
+        [ConfigurationProperty("subNames", IsKey = true, IsRequired = true)]
+        public string SubNames
+        {
+            get
+            {
+                return base["subnames"] as string;
+            }
+            set
+            {
+                base["subnames"] = value;
+            }
+        }
     }
 }
--- Gamification/DatabaseAccess/Configuration/UserMaps.cs
+++ Gamification/DatabaseAccess/Configuration/UserMaps.cs
@@ -3,10 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Configuration;
 namespace DatabaseAccess.Configuration
 {
-    public class UserMaps
+    [ConfigurationCollection(typeof(UserMap), AddItemName = "usermap", CollectionType = ConfigurationElementCollectionType.BasicMap)]
+    public class UserMaps : ConfigurationElementCollection, IEnumerable<UserMap>
     {
+
+        protected override ConfigurationElement CreateNewElement()
+        {
+            return new UserMap();
+        }
+
+        protected override object GetElementKey(ConfigurationElement element)
+        {
+            var l_configElement = element as UserMap;
+            if (l_configElement != null)
+                return l_configElement.MainName;
+            else
+                return null;
+        }
+
+        public UserMap this[int index]
+        {
+            get
+            {
+                return BaseGet(index) as UserMap;
+            }
+        }
+
+
+
+        IEnumerator<UserMap> IEnumerable<UserMap>.GetEnumerator()
+        {
+            return (from i in Enumerable.Range(0, this.Count)
+                    select this[i])
+                    .GetEnumerator();
+        }
+
     }
 }
